926c497dc9298d50ef55fda99d0810425cd839ed
Nick Ezell <alexsandratrammell@Nicks-MacBook-Pro.local>
2020-02-26 09:23:49 -0600

    Added axios to dependencies and installed node module.

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ import { prompt } from "inquirer";
 import { readFile } from "fs";
 import { appendFile } from "fs";
 import { promisify } from "util";
+import { axios } from "axios";
 
 const asyncWriteFile = promisify(appendFile);
