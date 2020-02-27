71988cb3e0ea28cafdc814d31cd72eba8ffd93b8
Nick Ezell <alexsandratrammell@Nicks-MacBook-Pro.local>
2020-02-27 10:20:13 -0600

    Began prompting for README data.

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const axios = require("axios");
 
 const asyncWriteFile = util.promisify(fs.appendFile);
 
-function getUserData() {
+function getUsername() {
     return inquirer
         .prompt([
             {
@@ -20,19 +20,35 @@ function getUserData() {
             }
         ])
 };
-function axiosCall(answers) {
-        const queryURL = `https://api.github.com/users/${answers.username}/events/public `;
-        axios
-            .get(queryURL).then(props => {
-                const userPicture = props.data[0].actor.avatar_url;
-                const userEmail = props.data[0].payload.commits[0].author.email;
-            })
+function getReadMeData(callData) {
+
+}
+function getReadMeData(answers) {
+    const queryURL = `https://api.github.com/users/${answers.username}/events/public `;
+    axios
+        .get(queryURL).then(props => {
+            const username = answers.username;
+            const userPicture = props.data[0].actor.avatar_url;
+            const userEmail = props.data[0].payload.commits[0].author.email;
+        })
+        .then(function () {
+            inquirer
+                .prompt([
+                    {
+                        type: "checkbox",
+                        name: "badges",
+                        message: "Great! Now let's choose some badges.",
+                        choices: ["Dependency Status", "Node License"]
+                    }
+                ])
+        })
 
 };
 async function init() {
     try {
-        const answers = await getUserData();
-        const callData = axiosCall(answers);
+        const answers = await getUsername();
+        const callData = getReadMeData(answers);
+        const readMe = await getReadMeData(callData);
         // const text = generateFile(answers);
     } catch (err) {
         console.log(err)
