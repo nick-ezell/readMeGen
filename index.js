b8cd5e51811c463e13a5abb4f22d533bf8c0f31c
Nick Ezell <alexsandratrammell@Nicks-MacBook-Pro.local>
2020-02-27 08:58:22 -0600

    Had to restart my work on the GitHub API call using a different search url.

--- index.js
+++ index.js
@@ -1,7 +1,42 @@
-import { prompt } from "inquirer";
-import { readFile } from "fs";
-import { appendFile } from "fs";
-import { promisify } from "util";
-import { axios } from "axios";
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
+const axios = require("axios");
 
-const asyncWriteFile = promisify(appendFile);
+const asyncWriteFile = util.promisify(fs.appendFile);
+
+function getUserData() {
+    return inquirer
+        .prompt([
+            {
+                type: "input",
+                name: "greetings",
+                message: "Salutations, fellow programmer! Got a name?"
+            },
+            {
+                type: "input",
+                name: "username",
+                message: `Nice to meet ya! Drop that GitHub username, friend. Let's get this README put together.`
+            }
+        ])
+};
+function axiosCall(answers) {
+        const queryURL = `https://api.github.com/users/${answers.username}/events/public `;
+        axios
+            .get(queryURL).then(props => {
+                const userPicture = props.data[0].actor.avatar_url;
+                console.log(props.data[0])
+            })
+
+};
+async function init() {
+    try {
+        const answers = await getUserData();
+        const callData = axiosCall(answers);
+        // const text = generateFile(answers);
+    } catch (err) {
+        console.log(err)
+    }
+};
+
+init();
